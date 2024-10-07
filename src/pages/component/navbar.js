c55c29332452b081870bd4443bc9434d8c74eb57
IFMSGio <119938552+IFMSGio@users.noreply.github.com>
2024-10-07 15:42:46 +0000

    aula 0710

--- src/pages/component/navbar.js
+++ src/pages/component/navbar.js
@@ -1,3 +1,18 @@
-export default function Navbar() {
-    return <div>Menu</div>
+import {Container, Nav, Navbar} from 'react-bootstrap';
+
+export default function Menu() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
